6c2fa76804939813672f952223623cdf5518c8ce
frank-Z <1262445244@qq.com>
2016-10-19 09:34:16 +0800

    Delete hehe.js

--- action/hehe.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by frank-z on 2016/9/8.
- * email : 1262445244@qq.com
- */
-defineAction("hehe",function(req,res){
-    res.myRender("haha.html",{"haha":"hahahahahahaha.....","title":"我是hehe"})
-})
